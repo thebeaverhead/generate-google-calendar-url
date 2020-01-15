9f5e044581dc1ffc155bdcc5960a3e8e02f74041
Piotr Pozniak <piotr.pozniak@thebeaverhead.com>
2020-01-15 14:11:49 +0200

    Added flag to ignore the Z at the end of the time string

--- generate-google-calendar-url.js
+++ generate-google-calendar-url.js
@@ -1,51 +1,72 @@
 (function() {
-  var moment = typeof module === 'object' ? require('moment') : window.moment,
-    BASE_URL = 'http://www.google.com/calendar/event?action=TEMPLATE',
+  var moment = typeof module === "object" ? require("moment") : window.moment,
+    BASE_URL = "http://www.google.com/calendar/event?action=TEMPLATE",
     MAX_LENGTH = 512,
     toMoment = function(options) {
-      return moment(moment(options.date, 'YYYY/MM/DD'));
+      return moment(moment(options.date, "YYYY/MM/DD"));
     },
     toAllDay = function(options) {
-      if (!options.date) return '';
+      if (!options.date) return "";
 
       var moment = toMoment(options);
 
-      return moment.isValid() ?
-        '&dates=' + moment.format('YYYYMMDD') + '/' + moment.add(1, 'd').format('YYYYMMDD') :
-        '';
+      return moment.isValid()
+        ? "&dates=" +
+        moment.format("YYYYMMDD") +
+        "/" +
+        moment.add(1, "d").format("YYYYMMDD")
+        : "";
     },
-    toIsoHour = function(date) {
-      return moment(date).utc().format('YYYYMMDDTHHmmss') + 'Z';
+    toIsoHour = function(date, ignoreTimezone = false) {
+      return (
+        moment(date)
+          .utc()
+          .format("YYYYMMDDTHHmmss") + (!ignoreTimezone ? "Z" : "")
+      );
     },
     toHour = function(options) {
-      if (!(options.start instanceof Date) || !(options.end instanceof Date)) return '';
+      if (!(options.start instanceof Date) || !(options.end instanceof Date))
+        return "";
 
-      return '&dates=' + toIsoHour(options.start) + '/' + toIsoHour(options.end);
+      const ignoreTimezone = options.ignoreTimezone ? true : false;
+
+      return (
+        "&dates=" +
+        toIsoHour(options.start, ignoreTimezone) +
+        "/" +
+        toIsoHour(options.end, ignoreTimezone)
+      );
     },
     toDatesParameter = function(options) {
-      return options.start && options.end ? toHour(options) :
-        options.date ? toAllDay(options) :
-        '';
+      return options.start && options.end
+        ? toHour(options)
+        : options.date
+          ? toAllDay(options)
+          : "";
     },
     toStringParameter = function(options, propertyName, alternativeName) {
-      if (!options[propertyName]) return '';
+      if (!options[propertyName]) return "";
 
-      return '&' +
+      return (
+        "&" +
         (alternativeName || propertyName) +
-        '=' +
-        encodeURIComponent(options[propertyName].substr(0, MAX_LENGTH - 1));
+        "=" +
+        encodeURIComponent(options[propertyName].substr(0, MAX_LENGTH - 1))
+      );
     },
     generateUrl = function(options) {
       options = options || {};
 
-      return BASE_URL +
-        toStringParameter(options, 'title', 'text') +
-        toStringParameter(options, 'location') +
-        toStringParameter(options, 'details') +
-        toDatesParameter(options);
+      return (
+        BASE_URL +
+        toStringParameter(options, "title", "text") +
+        toStringParameter(options, "location") +
+        toStringParameter(options, "details") +
+        toDatesParameter(options)
+      );
     };
 
-  if (typeof module === 'object') {
+  if (typeof module === "object") {
     // CommonJS
     module.exports = generateUrl;
   } else {
@@ -53,3 +74,4 @@
     window.generateUrl = generateUrl;
   }
 })();
+
